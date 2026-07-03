7b0f9410f360265682b6d512f63bbbd058a70845
jituman01 <naimulislamjitu585@gmail.com>
2026-07-03 20:24:33 +0600

    implement feature a toggle switch for Monthly vs Annually pricing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,105 @@
-import './App.css'
+import React, { useState } from 'react';
+import PremiumPricingCard from './components/PremiumPricingCard';
 
-function App() {
+export default function App() {
+  const [isAnnual, setIsAnnual] = useState(false);
+
+  const plans = [
+    {
+      title: 'Starter',
+      price: isAnnual ? 182 : 19,
+      features: [
+        'Basic Features',
+        'Email Support',
+        'Public Profile',
+        'Community Access',
+      ],
+      decs: 'Essential tools to kickstart your project with basic.',
+    },
+    {
+      title: 'Premium',
+      price: isAnnual ? 951 : 99,
+      features: [
+        'Everything in Starter',
+        'Priority Support',
+        'Advanced Analytics,',
+        'Dedicated Manager',
+        'Dedicated Server',
+        'Advanced Security Suite',
+        'User Training Sessions',
+        'Personalized Dashboard',
+      ],
+      isPopular: true,
+      decs: 'Unlock powerful analytics and priority access to scale your business.',
+    },
+    {
+      title: 'Enterprise',
+      price: isAnnual ? 470 : 49,
+      features: [
+        'Custom Solutions',
+        '24/7 Support',
+        'Dedicated Manager',
+        'Custom Branding',
+        'API Access',
+        'Standard Security',
+      ],
+      decs: 'Tailored, mid-performance solutions for large-scale operations.',
+    },
+  ];
 
   return (
-    <div className='text-2xl'>
-          Hello world
+    <div className="min-h-screen bg-gray-50 py-10 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto text-center">
+        <h1 className="text-4xl font-extrabold mb-5">Pricing Card</h1>
+        <p className="text-gray-500 text-lg mb-12">
+          Choose the perfect plan that fits your needs. Whether you're just
+          starting out or scaling your business, we have a solution for
+          you.Switch between monthly and annual billing to{' '}
+          <span className="text-yellow-500 font-semibold">save up to 20%</span>{' '}
+          on all plans. Cancel anytime, no questions asked.
+        </p>
 
+        <div className="text-center">
+          {/* Toggle */}
+          <div className="flex justify-center items-center mb-16 gap-4">
+            <span
+              className={`font-bold ${
+                !isAnnual ? 'text-blue-600' : 'text-gray-400'
+              }`}
+            >
+              Monthly
+            </span>
+            <button
+              onClick={() => setIsAnnual(!isAnnual)}
+              className="w-14 h-8 bg-blue-500 rounded-full p-1 flex items-center"
+            >
+              <div
+                className={`w-6 h-6 bg-white rounded-full ${
+                  isAnnual ? 'translate-x-6' : 'translate-x-0'
+                }`}
+              />
+            </button>
+            <span
+              className={`font-bold ${
+                isAnnual ? 'text-blue-600' : 'text-gray-400'
+              }`}
+            >
+              Annually
+            </span>
+          </div>
+
+          {/* Cards */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {plans.map((plan, i) => (
+              <PremiumPricingCard
+                key={i}
+                {...plan}
+                period={isAnnual ? '/year' : '/mo'}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
---
+++ src/components/PremiumPricingCard.jsx
@@ -0,0 +1,59 @@
+import { Check } from 'lucide-react';
+import React from 'react';
+
+const PremiumPricingCard = ({
+  title,
+  decs,
+  price,
+  features,
+  isPopular,
+  period,
+}) => {
+  return (
+    <div
+      className={`relative flex flex-col p-8 rounded-3xl border  duration-300 hover:scale-105  shadow-xl 
+      ${
+        isPopular
+          ? 'border-yellow-500 ring-4 ring-yellow-500/10 bg-white'
+          : 'border-gray-300 bg-white'
+      }`}
+    >
+      {/* Badge */}
+      {isPopular && (
+        <div className="absolute -top-3.5 left-1/2 -translate-x-1/2 rounded-full bg-yellow-500 px-4 py-1 text-xs font-bold text-white shadow-sm">
+          Most Popular
+        </div>
+      )}
+
+      <h3 className="text-2xl font-bold text-gray-900">{title}</h3>
+      <div className="mt-2 mb-8">
+        <p className=" text-sm text-gray-500 min-h-[60px]">{decs}</p>
+        <span className="text-4xl font-extrabold">${price}</span>
+        <span className="text-gray-500 font-medium">{period}</span>
+      </div>
+
+      <ul className="space-y-4 mb-8 flex-grow ">
+        {features.map((feature, i) => (
+          <li key={i} className="flex items-center gap-3 text-sm text-gray-600">
+            <div className="flex h-5 w-5 items-center justify-center rounded-full bg-yellow-50 text-yellow-500">
+              <Check size={14} strokeWidth={3} />
+            </div>
+            {feature}
+          </li>
+        ))}
+      </ul>
+      {/*  button */}
+      <button
+        className={`w-full py-3 rounded-full font-bold cursor-pointer ${
+          isPopular
+            ? 'bg-yellow-500 text-white hover:bg-yellow-600'
+            : 'bg-gray-900 text-white hover:bg-gray-800'
+        }`}
+      >
+        Choose Plan
+      </button>
+    </div>
+  );
+};
+
+export default PremiumPricingCard;
